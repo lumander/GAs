da84ebf6414e662e400ac476e56643bc50c3b5e9
lumander <alessandro.lumino@gmail.com>
2018-11-21 21:06:48 +0100

    code refactor

--- image-learning/Individual.py
+++ image-learning/Individual.py
@@ -38,25 +38,23 @@ class Individual():
         '''
         rectangles = []
         for i in range( 0, self.num_rectangles * self.alleles, self.alleles ):
-            up_left_vertex0 = self.string_decoding( self.binary_encoding[ i : i+8 ] )
-            up_left_vertex1 = self.string_decoding( self.binary_encoding[ i+8 : i+16 ] )
-            down_right_vertex0 = self.string_decoding( self.binary_encoding[ i+16 : i+24 ] )
-            down_right_vertex1 = self.string_decoding( self.binary_encoding[ i+24 : i+32 ] )
-            red = self.string_decoding( self.binary_encoding[ i+32 : i+40 ] )
-            green = self.string_decoding( self.binary_encoding[ i+40 : i+48 ] )
-            blue = self.string_decoding( self.binary_encoding[ i+48 : i+56 ] )
+            up_left_vertex0 = self.fromGrayCode( self.binary_encoding[ i : i+8 ] )
+            up_left_vertex1 = self.fromGrayCode( self.binary_encoding[ i+8 : i+16 ] )
+            down_right_vertex0 = self.fromGrayCode( self.binary_encoding[ i+16 : i+24 ] )
+            down_right_vertex1 = self.fromGrayCode( self.binary_encoding[ i+24 : i+32 ] )
+            red = self.fromGrayCode( self.binary_encoding[ i+32 : i+40 ] )
+            green = self.fromGrayCode( self.binary_encoding[ i+40 : i+48 ] )
+            blue = self.fromGrayCode( self.binary_encoding[ i+48 : i+56 ] )
             rectangle = Rectangle( up_left_vertex0, up_left_vertex1, down_right_vertex0, down_right_vertex1, red, green, blue )
             rectangles.append( rectangle )
     
         return rectangles
 
-    def string_decoding( self, genes ): 
+    def fromGrayCode( self, genes ): 
 
         '''
-        Performs binary to decimal conversion of the genes
+        Performs conversion from Gray Code to decimal representation
         '''
-
-        
 
         _xor = {("0", "0"): "0",
         ("0", "1"): "1",
@@ -81,6 +79,11 @@ class Individual():
         self.rectangles = self.decoding()
 
     def toGrayCode( self ):
+
+        '''
+        Performs conversion decimal representation to Gray Code 
+        '''
+
         _xor = {("0", "0"): "0",
         ("0", "1"): "1",
         ("1", "0"): "1",
